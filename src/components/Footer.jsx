30bd3855f0bcb3cb70307fc9ee72864ab15cc5d5
surya-prakash603 <suryaraj04266@gmail.com>
2024-02-24 22:11:34 +0530

    ..

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,8 +14,8 @@ function Footer() {
     <h6 className="footer-title">Company</h6> 
     <a className="link link-hover">About us</a>
     <a className="link link-hover">Contact</a>
-    <a className="link link-hover">Jobs</a>
-    <a className="link link-hover">Press kit</a>
+    <a className="link link-hover">Instgram</a>
+    <a className="link link-hover">LinkedIn</a>
   </nav> 
   <nav>
     <h6 className="footer-title">Social</h6> 
