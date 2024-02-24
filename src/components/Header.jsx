8edbf87f4a993773ce0e361f32f0616de61259e8
surya-prakash603 <suryaraj04266@gmail.com>
2024-02-24 22:09:57 +0530

    ..

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Layout from './Layout';
 
 function Header(props) {
-  const [inputvalue, setinputvalue] = useState('all');
+  const [inputvalue, setinputvalue] = useState('Hollywood');
   const [state,setstate]=useState("light")
   console.log(state);
   const handlemode=()=>{
@@ -76,29 +76,29 @@ function Header(props) {
         {/* Carousel part starts */}
 
         <div className="carousel w-full h-96 mb-20">
-  <div id="slide1" className="carousel-item relative w-full h-5/6">
-    <img src="https://wallpaperaccess.com/full/1288436.jpg" className="w-full" />
+  <div id="slide1" className="carousel-item relative w-full">
+    <img src="https://wallpaperaccess.com/full/37963.jpg" className="w-full" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide4" className="btn btn-circle">❮</a> 
       <a href="#slide2" className="btn btn-circle">❯</a>
     </div>
   </div> 
   <div id="slide2" className="carousel-item relative w-full">
-    <img src="https://daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg" className="w-full" />
+    <img src="https://wallpaperaccess.com/full/37945.jpg" className="w-full" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide1" className="btn btn-circle">❮</a> 
       <a href="#slide3" className="btn btn-circle">❯</a>
     </div>
   </div> 
   <div id="slide3" className="carousel-item relative w-full">
-    <img src="https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg" className="w-full" />
+    <img src="https://wallpaperaccess.com/full/37962.jpg" className="w-full" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide2" className="btn btn-circle">❮</a> 
       <a href="#slide4" className="btn btn-circle">❯</a>
     </div>
   </div> 
   <div id="slide4" className="carousel-item relative w-full">
-    <img src="https://daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" className="w-full" />
+    <img src="https://wallpaperaccess.com/full/1288436.jpg" className="w-full" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide3" className="btn btn-circle">❮</a> 
       <a href="#slide1" className="btn btn-circle">❯</a>
